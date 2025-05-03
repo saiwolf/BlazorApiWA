0afdad32b8edc8cacb70dd0cbf190677f5f966f9
Robert Cato <saiwolf@swmnu.net>
2025-05-03 18:16:29 -0500

    Documented WeatherForecast class

--- BlazorApiWA/Shared/WeatherForecast.cs
+++ BlazorApiWA/Shared/WeatherForecast.cs
@@ -1,15 +1,36 @@
 ﻿namespace BlazorApiWA.Shared;
 
+/// <summary>
+/// Class that represents example forecast data.
+/// </summary>
 public class WeatherForecast
 {
-    public DateTime Date { get; set; }
+    /// <summary>
+    /// The date of the forecast.
+    /// </summary>
+    public required DateTime Date { get; set; }
 
+    /// <summary>
+    /// The temperature of the forecast in degrees celsius.
+    /// </summary>
     public int TemperatureC { get; set; }
 
+    /// <summary>
+    /// <para>The temperature of the forecast in degrees fahrenheit.</para>
+    /// <para>
+    /// Property is <c>get</c> only and uses <see cref="TemperatureC"/> to calculate value.
+    /// </para>
+    /// </summary>
     public int TemperatureF => (TemperatureC * 9) / 5 + 32;
 
+    /// <summary>
+    /// A one-word summary of the forecast.
+    /// </summary>
     public string Summary { get; set; } = default!;
 
+    /// <summary>
+    /// List of one-word summaries to use for <see cref="Summary"/>.
+    /// </summary>
     public static readonly string[] Summaries =
 [
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
