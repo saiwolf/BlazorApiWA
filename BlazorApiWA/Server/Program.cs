a2b46935d74304d56ec54464e29ab342a209907d
Robert Cato <saiwolf@swmnu.net>
2025-05-03 13:49:10 -0500

    Add project files.

---
+++ BlazorApiWA/Server/Program.cs
@@ -0,0 +1,44 @@
+using BlazorApiWA.Shared;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.MapGet("/api/weather-forecast", Ok<WeatherForecast[]> () =>
+{
+    Random rng = new();
+    var forecasts = Enumerable.Range(1, 10).Select(index => new WeatherForecast
+    {
+        Date = DateTime.Now.AddDays(index),
+        TemperatureC = rng.Next(-20, 55),
+        Summary = WeatherForecast.Summaries[rng.Next(WeatherForecast.Summaries.Length)]
+    }).ToArray();
+
+    return TypedResults.Ok(forecasts);
+});
+
+app.Run();
---
+++ BlazorApiWA/Shared/WeatherForecast.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorApiWA.Shared;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => (TemperatureC * 9) / 5 + 32;
+
+    public string Summary { get; set; } = default!;
+
+    public static readonly string[] Summaries =
+[
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    ];
+}
