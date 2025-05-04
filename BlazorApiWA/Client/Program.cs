00cf9f29892a75bd57c1b9079f5f48ebf1ee7997
Robert Cato <saiwolf@swmnu.net>
2025-05-04 18:52:51 -0500

    Added Serilog to client project

--- BlazorApiWA/Client/Program.cs
+++ BlazorApiWA/Client/Program.cs
@@ -1,11 +1,23 @@
 using BlazorApiWA.Client.Components;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Serilog;
+using Serilog.Core;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+var levelSwitch = new LoggingLevelSwitch();
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.ControlledBy(levelSwitch)
+    .Enrich.WithProperty("InstanceId", Guid.NewGuid().ToString("n"))
+    .WriteTo.BrowserConsole()
+    .CreateLogger();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Logging.ClearProviders();
+builder.Services.AddLogging(lb => lb.AddSerilog(dispose: true));
 builder.Services.AddHttpClient("BlazorApiWA.ServerAPI", client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
