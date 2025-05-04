c450b1d00318f45444bd61fa6e97bf1474e7e286
Robert Cato <saiwolf@swmnu.net>
2025-05-04 18:45:50 -0500

    Add Serilog to Server project

--- BlazorApiWA/Server/Program.cs
+++ BlazorApiWA/Server/Program.cs
@@ -1,44 +1,66 @@
 using BlazorApiWA.Shared;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.ResponseCompression;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration().CreateBootstrapLogger();
 
-builder.Services.AddControllers();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseWebAssemblyDebugging();
-}
-else
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+    var builder = WebApplication.CreateBuilder(args);
+    IServiceCollection services = builder.Services;
 
-app.UseHttpsRedirection();
-app.UseBlazorFrameworkFiles();
-app.UseStaticFiles();
+    builder.Host.UseSerilog((ctx, lc) => lc
+        .ReadFrom.Configuration(ctx.Configuration));
 
-app.UseRouting();
+    services.AddControllers();
 
-app.MapControllers();
-app.MapFallbackToFile("index.html");
+    var app = builder.Build();
 
-app.MapGet("/api/weather-forecast", Ok<WeatherForecast[]> () =>
-{
-    Random rng = new();
-    var forecasts = Enumerable.Range(1, 10).Select(index => new WeatherForecast
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
     {
-        Date = DateTime.Now.AddDays(index),
-        TemperatureC = rng.Next(-20, 55),
-        Summary = WeatherForecast.Summaries[rng.Next(WeatherForecast.Summaries.Length)]
-    }).ToArray();
+        app.UseWebAssemblyDebugging();
+    }
+    else
+    {
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
 
-    return TypedResults.Ok(forecasts);
-});
+    app.UseHttpsRedirection();
 
-app.Run();
+    app.UseSerilogRequestLogging();
+
+    app.UseBlazorFrameworkFiles();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.MapControllers();
+    app.MapFallbackToFile("index.html");
+
+    app.MapGet("/api/weather-forecast", Ok<WeatherForecast[]> () =>
+    {
+        Random rng = new();
+        var forecasts = Enumerable.Range(1, 10).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = rng.Next(-20, 55),
+            Summary = WeatherForecast.Summaries[rng.Next(WeatherForecast.Summaries.Length)]
+        }).ToArray();
+
+        return TypedResults.Ok(forecasts);
+    });
+
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "An unhandled exception has occurred.\n{exMessage}", ex.Message);
+    Environment.Exit(-1);
+}
+finally
+{
+    await Log.CloseAndFlushAsync();
+}
